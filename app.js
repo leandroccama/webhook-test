7ea0d3642a0b18bb4e0e88943ff5621fc62c476d
leandroccama <66843278+leandroccama@users.noreply.github.com>
2026-04-16 10:36:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
-  console.log(JSON.stringify(req.body, null, 2));
+  console.log('Headers:', JSON.stringify(req.headers, null, 2));
+  console.log('Body:', JSON.stringify(req.body, null, 2));
   res.status(200).end();
 });
 
